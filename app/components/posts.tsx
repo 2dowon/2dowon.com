9d0a395ad912ac00c0468aaf256c5cb7bf5944f7
2dowon <dwon424@gmail.com>
2024-09-18 23:08:05 +0900

    chore: updates blog setting

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import { formatDate, getBlogPosts } from "app/posts/utils";
+import Link from "next/link";
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+  let allBlogs = getBlogPosts();
 
   return (
     <div>
@@ -11,26 +11,26 @@ export function BlogPosts() {
           if (
             new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
           ) {
-            return -1
+            return -1;
           }
-          return 1
+          return 1;
         })
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            className="flex flex-col mb-4 space-y-1"
+            href={`/posts/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <div className="flex flex-col w-full space-x-0 md:flex-row md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="tracking-tight text-neutral-900 dark:text-neutral-100">
                 {post.metadata.title}
               </p>
             </div>
           </Link>
         ))}
     </div>
-  )
+  );
 }
