ead72ed1d4ee8a0f19a5a4171f9b8e4d6dc03d65
2dowon <dwon424@gmail.com>
2024-09-20 22:36:04 +0900

    style: add freesentation font

---
+++ app/_fonts/font.ts
@@ -0,0 +1,42 @@
+import localFont from "next/font/local";
+
+export const FreesentationFont = localFont({
+  src: [
+    {
+      path: "./Freesentation-1Thin.ttf",
+      weight: "100",
+    },
+    {
+      path: "./Freesentation-2ExtraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "./Freesentation-3Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./Freesentation-4Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./Freesentation-5Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./Freesentation-6SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "./Freesentation-7Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "./Freesentation-8ExtraBold.ttf",
+      weight: "800",
+    },
+    {
+      path: "./Freesentation-9Black.ttf",
+      weight: "900",
+    },
+  ],
+});
