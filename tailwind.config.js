041f57acf8450d4571c4b3c987682f99cc612fbb
2dowon <dwon424@gmail.com>
2024-10-20 23:19:43 +0900

    style: add screens to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   darkMode: "selector",
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: { colors: { "primary-bg": "#171717", "primary-blue": "#153A8B" } },
+    extend: {
+      colors: { "primary-bg": "#171717", "primary-blue": "#153A8B" },
+      screens: {
+        pc: "600px",
+        mobile: { min: "1px", max: "599px" },
+      },
+    },
   },
   plugins: [],
 };
